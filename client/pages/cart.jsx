a664d3313cf9fb17fba23abd94e52f6eef2cecb9
Quan Pham <mockanterr@gmail.com>
2022-11-14 23:43:37 +0000

    sent token to verify and get cart items

--- client/pages/cart.jsx
+++ client/pages/cart.jsx
@@ -1,17 +1,43 @@
+import React from 'react';
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: null
+    };
+  }
 
-function Cart() {
-  const token = localStorage.getItem('userToken');
-  if (!token) {
-    fetch('/api/cart/token', {
-      method: 'POST'
-    })
-      .then(r => r.json())
-      .then(r => {
-        const { token } = r;
-        localStorage.setItem('token', token);
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      fetch('/api/cart/token', {
+        method: 'POST'
       })
-      .catch(err => console.error(err))
-    ;
+        .then(r => r.json())
+        .then(r => {
+          const { token } = r;
+          localStorage.setItem('token', token);
+        })
+        .catch(err => console.error(err))
+      ;
+    } else {
+      fetch('/api/cart/token', {
+        method: 'POST',
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+        .then(r => r.json())
+        .then(r => {
+          const cart = [...r];
+          this.setState({ cart });
+        })
+        .catch(err => console.error(err));
+    }
+  }
+
+  render() {
+    return <main><h1>hi</h1></main>;
   }
 }
 
