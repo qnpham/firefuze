c8d88fa4efeb498ff3f8d7c9231f91a87a58af93
Quan Pham <mockanterr@gmail.com>
2022-11-18 22:21:11 +0000

    adjusted confirmation message

--- client/pages/confirmation.jsx
+++ client/pages/confirmation.jsx
@@ -5,7 +5,7 @@ function Message() {
     <div className="container">
       <div className="confirmation-container">
         <h2 className='thank-you'>Thank you!</h2>
-        <p className='message' >Your receipt along with your products have been sent to the provided email.</p>
+        <p className='message' >Your order has successfully been processed. Your receipt along with your products have been sent to the provided email.</p>
       </div>
     </div>
   );
